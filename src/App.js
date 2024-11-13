014ec95243d233c555f65525503533ef05e43a2c
Henok Tilahun Lakew <henoktilahun363@gmail.com>
2024-11-13 11:01:20 +0300

    initial add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import '../src/dist/styles.css'
+import Navbar from './components/NavBar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import MarketUpdate from './pages/Market/MarketUpdate';
+import WhyUs from './pages/WhyUs/WhyUs';
+import Trade from './pages/Trade/Trade';
+import Coin from './pages/Coin/Coin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/market" element={<MarketUpdate />} />
+        <Route path="/choose-us" element={<WhyUs />} />
+        <Route path="/trade" element={<Trade />} />
+        <Route path='/coin' element={ <Coin /> }>
+          <Route path=':coinId' element={ <Coin /> } />
+        </Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 

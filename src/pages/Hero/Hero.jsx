7adddbc63c1d95efad9a90a4b2143965ff835841
Henok Tilahun Lakew <henoktilahun363@gmail.com>
2024-12-29 20:29:02 +0300

    some change on the wallet

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import './wallet.css'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaArrowUp } from "react-icons/fa6";
+import { FaArrowDown } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa";
+import { RiBankLine } from "react-icons/ri";
+import { LuFolderClock } from "react-icons/lu";
 import {
   IconChevronDown,
   IconUpload,
@@ -11,12 +20,17 @@ import {
 import axios from "axios";
 import Btc from "../../images/hero/bitcoin.png";
 import Eth from "../../images/hero/ethereum.png";
+import Login from "../../components/Login/Login";
+
+
+
 
 function Hero() {
   const [data, setData] = useState([]);
   const [coinsLoad, setCoinsLoad] = useState(true);
   const [totalAmount, setTotalAmount] = useState(null);
   const [userMessage, setUserMessage] = useState("");
+  const [isValueVisible, setIsValueVisible] = useState(false);
 
   const marketUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false`;
   const apiBaseUrl = "https://crypto-ault.onrender.com/api/auth";
@@ -62,6 +76,11 @@ function Hero() {
     fetchWalletTotal();
   }, [marketUrl, apiBaseUrl]);
 
+  // Toggle the visibility of the price value
+  const toggleVisibility = () => {
+    setIsValueVisible(!isValueVisible);
+  };
+
   return (
     <>
       {/* First Section */}
@@ -69,28 +88,87 @@ function Hero() {
         <div className="container">
           <div className="hero-content">
             <div className="hero-content__text" style={{ marginTop: "100px" }}>
-              <img className="btc-float" src={Btc} alt="floating-el" />
+              {/* <img className="btc-float" src={Btc} alt="floating-el" /> */}
               {userMessage ? (
                 <h6 className="user-message">{userMessage}</h6>
               ) : (
-                <h6>
-                  <span>Main Wallet Total:</span>
-                  <br />
-                  <span className="wallet-total">
-                    {numberWithCommas(totalAmount)}
-                    <br />$
-                  </span>
-                  <span>Crypto currencies</span>
-                </h6>
+                // <h6>
+                //   <span>Main Wallet Total:</span>
+                //   <br />
+                //   <span className="wallet-total">
+                //     {numberWithCommas(totalAmount)}
+                //     <br />$
+                //   </span>
+                //   <span>Crypto currencies</span>
+                // </h6>
+                <div>
+                  <div className="wallet-container">
+                    <div className="inner-wallet">
+                      Main Wallet 1
+                      <FaAngleDown className="inner" />
+                    </div>
+                    <div className="price">
+                      {isValueVisible ? (
+                        <p className="price-value">$ {totalAmount}</p>
+                      ) : (
+                        <p className="price-value">{"•••••"}</p>
+                      )}
+                      {/* <p className="price-value">
+                        {isValueVisible ? {totalAmount} : "•••••"}
+                      </p> */}
+                      {/* {isValueVisible ? "$0.00" : "•••••"} */}
+                      {/* <FaEye /> */}
+                      <button
+                        onClick={toggleVisibility}
+                        style={{
+                          background: "none",
+                          border: "none",
+                          cursor: "pointer",
+                          padding: "0",
+                          color: "white",
+                        }}
+                      >
+                        {isValueVisible ? (
+                          <FaEyeSlash size={18} />
+                        ) : (
+                          <FaEye size={18} />
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                  <div className="actions" style={{ color: "white" }}>
+                    <div className="buttons">
+                      <FaArrowUp className="icons" size={36} />
+                      <p className="text">Send</p>
+                    </div>
+
+                    <div className="buttons">
+                      <FaArrowDown className="icons" size={36} />
+                      <p className="text">Receive</p>
+                    </div>
+                    <div className="buttons">
+                      <FaWallet className="icons" size={36} />
+                      <p className="text">Buy</p>
+                    </div>
+                    <div className="buttons">
+                      <RiBankLine className="icons" size={36} />
+                      <p className="text">Sell</p>
+                    </div>
+                    <div className="buttons">
+                      <LuFolderClock className="icons" size={36} />
+                      <p className="text">History</p>
+                    </div>
+                  </div>
+                </div>
               )}
-              <img className="eth-float" src={Eth} alt="floating-el" />
+              {/* <img className="eth-float" src={Eth} alt="floating-el" /> */}
             </div>
 
-            <a className="mobile-btn-hero" href="#market">
+            {/* <a className="mobile-btn-hero" href="#market">
               See Prices <IconChevronDown />
-            </a>
+            </a> */}
 
-            <div onLoad={() => setCoinsLoad(false)} className="coin-slider">
+            {/* <div onLoad={() => setCoinsLoad(false)} className="coin-slider">
               {coinsLoad && <span className="loader"></span>}
               {data.map((item) => (
                 <Link
@@ -117,13 +195,13 @@ function Hero() {
                   </p>
                 </Link>
               ))}
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
 
       {/* Second Section */}
-      <section id="home" className="hero-section1">
+      {/* <section id="home" className="hero-section1">
         <div className="container">
           <div className="hero-actions">
             <Link to="/wallet" className="action">
@@ -148,7 +226,7 @@ function Hero() {
             </Link>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Styling */}
       <style jsx>{`
